36d521cbd551dcb8f7aaeebaf5cd2fbb9b3a3a7e
zdy023 <zdy004007@126.com>
2023-06-07 15:53:33 +0800

    ver Jun7thv2
    rerunning experiments for ChatGPT

--- launch.sh
+++ launch.sh
@@ -2,10 +2,10 @@
 
 export TOKENIZERS_PARALLELISM=false
 python main.py --task-path ../../../android_env/apps/wikihow/templates.miniout.microbuffer\
-			   --avd-name Pixel_2_API_30_ga_x64\
+			   --avd-name Pixel_2_API_30_ga_x64_1\
 			   --tokenizer-path weights/vilt-b32-mlm-tiny-tkn\
-			   --prompt-template prompts/prompt_pt_2k.txt\
+			   --prompt-template prompts/chat_prompt\
 			   --request-timeout 8.\
-			   --model llama-13b\
-			   --starts-from 12\
+			   --model gpt-3.5-turbo\
+			   --starts-from 0\
 			   --ends-at 70
